1e5b57f7b67144cc53bc0a396c4c7b922f20bcf1
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-07 19:57:42 -0300

    Perfil de teste criado 9 V1

--- src/main/java/com/luciano/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/luciano/helpdesk/HelpdeskApplication.java
@@ -1,53 +1,13 @@
 package com.luciano.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.luciano.helpdesk.domain.Chamado;
-import com.luciano.helpdesk.domain.Cliente;
-import com.luciano.helpdesk.domain.Tecnico;
-import com.luciano.helpdesk.domain.enums.Perfil;
-import com.luciano.helpdesk.domain.enums.Prioridade;
-import com.luciano.helpdesk.domain.enums.Status;
-import com.luciano.helpdesk.repositories.ChamadoRepository;
-import com.luciano.helpdesk.repositories.ClienteRepository;
-import com.luciano.helpdesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
+public class HelpdeskApplication{
 	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "13220629784", "valdir@gmail.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cli1 = new Cliente(null, "Linus T", "13220629734", "linux@gmail.com", "123");
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
-	
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
-	
-	
 	}
 
 }
