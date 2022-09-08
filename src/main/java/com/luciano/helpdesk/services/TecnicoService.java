a24d9d7a9d4c8c833752f7d75770b1c5306ca896
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-08 19:45:25 -0300

    endpoint create para Tecnico 15 V1

--- src/main/java/com/luciano/helpdesk/services/TecnicoService.java
+++ src/main/java/com/luciano/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luciano.helpdesk.domain.Tecnico;
+import com.luciano.helpdesk.domain.dtos.TecnicoDTO;
 import com.luciano.helpdesk.repositories.TecnicoRepository;
 import com.luciano.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 }
