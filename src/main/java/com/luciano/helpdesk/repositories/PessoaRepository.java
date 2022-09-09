f9e04426380e9cc9c4c1cb8a691cc86d404507fc
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-09-09 06:50:56 -0300

    exceção para CPF e EMAIL cadastradas 16 V1

--- src/main/java/com/luciano/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/luciano/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.luciano.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luciano.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
+	
 }
